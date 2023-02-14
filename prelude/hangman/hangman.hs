d97624f4085994a149f14918a7e397e4483dc276
kukimik <kukimik@poczta.onet.pl>
2023-02-14 21:38:57 +0100

    todo comments

--- prelude/hangman/hangman.hs
+++ prelude/hangman/hangman.hs
@@ -1,16 +1,25 @@
 #!/usr/bin/env runghc
 {-# LANGUAGE CPP #-}
-wordList = words "shuffle jazzy weather emanate microbe"
+wordList = words "shuffle jazzy weather emanate microbe cohomology monad functor applicative bane same sane game lame"
 w = wordList !! ((`mod` length wordList) . read  . (filter (/= ':')) $ __TIME__)
 
+--- cycling through the whole list by period s.t. length mod period = 1
+-- or: after 5 words: "Enough playing, go back to work!"
+
 h = "  ___\n |  \\|\n o   |\n-+-  |\n/ \\  |\n   __|__\n"
-m = (read . (\x->[x])) <$> "0033300500420060002087800209090020000112110"
+m = (read . (\x->[x])) <$> "0033300500420060002087800209090020000112110" -- use order on chars to get 11 moves?
 u k a b = if b <= k then a else ' '
 
-main = game 0 (replicate (length w) '.')
+main = game 0 (replicate (length w) '.') -- use the interact "trick"
 
 f x a '.' = if a == x then a else '.'
 f _ _ b = b
+
+compose :: [r -> r] -> r -> r
+compose = flip (foldl (flip id))
+
+toUpper c = if elem c ['A'..'Z'] then compose (replicate 32 succ) c else c
+
 
 game 9 _ = putStrLn "You lose!"
 game n a = do
