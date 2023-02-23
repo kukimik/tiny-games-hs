f2dda1b5701ea358763fbd0b03df3b7a4c67fb38
kukimik <kukimik@poczta.onet.pl>
2023-02-23 23:03:44 +0100

    work

--- prelude/hangman/hangman.hs
+++ prelude/hangman/hangman.hs
@@ -1,45 +1,44 @@
 #!/usr/bin/env runghc
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE CPP #-}{-# LANGUAGE MultiWayIf #-}
 w = wordList !! ((`mod` length wordList) . read  . (filter (/= ':')) $ __TIME__)
 
 --- cycling through the whole list by period s.t. length mod period = 1
 -- or: after 5 words: "Enough playing, go back to work!"
 
 chars = ' ':['a'..'z']
-triples = sequence [chars,chars,chars]
+triples = sequence [chars,chars,chars] -- why only chars here? maybe use larger set and compress the hangman picture?
 wordList = words . concat $ map (\x-> triples !! ((fromEnum x) - 2^17)) "𣜈𡇄𠹇𠖱𤝈𠹬𡝡𠂔𠑔𣥻𢗻𣓙𠹀𢮞𢚦𢮍𠅮𢟽𠂷𢡃𢲝𠒙𢌢𠓾𣼭𠗛𠹐𠐽𠈂𢡥𡐗𠹉𠐽" -- 2^17 = 0x20000; U+20000 - początek długiego bloku CJK (U+20000 - U+2A6DF)
 
 h = "  ___\n |  \\|\n o   |\n-+-  |\n/ \\  |\n   __|__\n"
-m = "003330050042006000208790020:0;0020000112110" -- use order on chars to get 11 moves?
+m = "003330050042006000208790020:0;0020000112110" -- use characters and compress!
 u k a b = if b <= toEnum (k + 48) then a else ' '
 
-main = game 0 (replicate (length w) '.') -- use the interact "trick"
+main = game 0 ('.'<$w) -- use the interact "trick"  interact $ go 0 (replicate (length w) '.') w 
 
 f x a '.' = if a == x then a else '.'
 f _ _ b = b
 
-compose :: [r -> r] -> r -> r
-compose = flip (foldl (flip id))
-
-nSucc n x = foldl (flip id) x (replicate n succ)
-
-toUpper c = if elem c ['A'..'Z'] then compose (replicate 32 succ) c else c
+toLower c = if elem c ['A'..'Z'] then shift 32 c else c
 
 i a b c = if a then b else c
 
-game 11 _ = putStrLn "You lose!"
+shift n = toEnum . (+ n) . fromEnum
+
+go n a w (c:cs) = 
+  "\ESC[2J"++(zipWith (u n) h m)++a++"\n"
+  ++ if | n==11 -> "You lose!" 
+        | elem '.' a -> "Choose a letter. Wrong guesses: " ++ (show n) ++ "/11." ++ (go (n + if elem c w then 0 else 1) (zipWith (f c) w a) w cs)
+        | otherwise -> "\nYou win!"
+
 game n a = do
-  putStrLn $ "\ESC[2J"++(zipWith (u n) h m)++a++"\n"
-  if elem '.' a
-    then
+  putStrLn $ "\ESC[2J"++(zipWith (u n) h m)++'\n':a++"\n"
+  if | n == 11 -> putStrLn "\nYou lose!"
+     | elem '.' a -> 
       do
-        putStrLn $ "Choose a letter. Guesses: " ++ (show n) ++ "/11."
-        ll <- getLine
-        -- TODO: handle ll length (should be 1, and should be a char!), handle toUpper
-        let l = head ll
-        game (n + if elem l w then 0 else 1) (zipWith (f l) w a)
-    else
-      putStrLn $ a ++ "\nYou win!"
+        putStrLn $ "Choose a letter. Wrong guesses: " ++ (show n) ++ "/11."
+        l:m <- ((fmap toLower)<$>) $ getLine
+        game (n + fromEnum (m\=[]&&elem l ['a'..'z']&&elem l w)) (zipWith (f l) w a)
+     | 1>0 -> putStrLn "\nYou win!"
 
 {-
   ___\n
