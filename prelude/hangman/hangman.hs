083a04a41ad8037136548148bddf0f05ff99e6de
kukimik <kukimik@poczta.onet.pl>
2023-02-26 17:29:00 +0100

    golfing up a bit

--- prelude/hangman/hangman.hs
+++ prelude/hangman/hangman.hs
@@ -1,22 +1,24 @@
 #!/usr/bin/env runghc
 {-# LANGUAGE CPP #-}{-# LANGUAGE MultiWayIf #-}
-w = wordList !! ((`mod` length wordList) . read  . (filter (/= ':')) $ __TIME__)
-
+w = wordList !! ((`mod` length wordList) . read $ [x|x<-__TIME__,x/=':'])
+decode=(>>=(\x->triples!!((fromEnum x)-2^17)))
 --- cycling through the whole list by period s.t. length mod period = 1
 -- or: after 5 words: "Enough playing, go back to work!"
 
 chars = ' ':['a'..'z']
-triples = sequence [chars,chars,chars] -- why only chars here? maybe use larger set and compress the hangman picture?
-wordList = words . concat $ map (\x-> triples !! ((fromEnum x) - 2^17)) "𣜈𡇄𠹇𠖱𤝈𠹬𡝡𠂔𠑔𣥻𢗻𣓙𠹀𢮞𢚦𢮍𠅮𢟽𠂷𢡃𢲝𠒙𢌢𠓾𣼭𠗛𠹐𠐽𠈂𢡥𡐗𠹉𠐽" -- 2^17 = 0x20000; U+20000 - początek długiego bloku CJK (U+20000 - U+2A6DF)
+triples = mapM id=<<[[chars,chars,chars],[chars,chars],[chars]] -- why only chars here? maybe use larger set and compress the hangman picture?
+
+wordList = words . decode $ "𣜈𡇄𠹇𠖱𤝈𠹬𡝡𠂔𠑔𣥻𢗻𣓙𠹀𢮞𢚦𢮍𠅮𢟽𠂷𢡃𢲝𠒙𢌢𠓾𣼭𠗛𠹐𠐽𠈂𢡥𡐗𠹉𠐽" -- 2^17 = 0x20000; U+20000 - początek długiego bloku CJK (U+20000 - U+2A6DF)
 
 h = "  ___\n |  \\|\n o   |\n-+-  |\n/ \\  |\n   __|__\n"
-m = "003330050042006000208790020:0;0020000112110" -- use characters and compress!
-u k a b = if b <= toEnum (k + 48) then a else ' '
+m = "aadddaafaaecaagaaacaihjaacakalaacaaaabbcbba" -- use characters and compress!
+
+u k a b|b <= toEnum (k + 97)=a|1>0=' '
 
 main = game 0 ('.'<$w)
 
-f x a '.' = if a == x then a else '.'
-f _ _ b = b
+f x a '.'|a==x=a|1>0='.'
+f _ _ b=b
 
 toLower c = if elem c ['A'..'Z'] then shift 32 c else c
 
