a123710ddfe92a1c95459e26a3f2723a684c49dc
Michal Kukiela <michal.kukiela@mga.com.pl>
2023-02-28 00:08:39 +0100

    golfing...

--- prelude/hangman/gen_string.hs
+++ prelude/hangman/gen_string.hs
@@ -1,16 +1,43 @@
 import Data.List(elemIndex)
 import Data.Maybe(fromJust)
+import Data.Char(isPrint)
 
-chars = ' ':['a'..'z']
-triples = sequence [chars,chars,chars]
-wordList = "shuffle jazzy weather emanate microbe cohomology monad functor applicative bane same sane game lame"
+chars = ['a'..'z']++" /?.:YWEPCB"
+triples = mapM id=<<[[chars,chars,chars]]
+
+tri :: String -> [String]
 tri (a:b:c:l) = [a,b,c]:(tri l)
 tri [] = []
 
-encoded = (encode <$> tri wordList)
+encodeTriple :: Int -> String -> Char
+encodeTriple n = (\t -> toEnum . (+ n) . fromJust $ elemIndex t triples)
 
-encode :: String -> Char
-encode = (\t -> toEnum . (+ 0x20000) . fromJust $ elemIndex t triples)
-main = putStrLn (encode <$> tri wordList) >> putStrLn (concat $ (map (\x-> triples !! ((fromEnum x) - 0x20000)) "𣜈𡇄𠹇𠖱𤝈𠹬𡝡𠂔𠑔𣥻𢗻𣓙𠹀𢮞𢚦𢮍𠅮𢟽𠂷𢡃𢲝𠒙𢌢𠓾𣼭𠗛𠹐𠐽𠈂𢡥𡐗𠹉𠐽"))
+encode :: Int -> String -> String 
+encode n x = (encodeTriple n <$> tri x)
+
+strings =
+  [
+   "shuffle jazzy weather emanate microbe cohomology monad functor applicative bane same sane game lame"
+  ,"You win. "
+  ,"You lose."
+  ,"aadddaafaaecaagaaacaihjaacakalaacaaaabbcbbaaa"
+  ,"Wrong guesses: "
+  ,"Enough playing. Back to work. "
+  ,"Play again? y/n"
+  ,"Enter a letter."
+  ]
+
+x n = (encode n) <$> strings
+
+try n = 
+  case (filter (not . isPrint) $ concat (x n)) of
+    [] -> putStrLn . unlines $ (x n)
+    u -> do
+          putStrLn $ (show n)++":"++(show $ ((triples !!).(+ (-n)).fromEnum) <$> u)
+          try (n+1)
+
+main = try 256
+
+  
 
 
--- prelude/hangman/hangman.hs
+++ prelude/hangman/hangman.hs
@@ -1,53 +1,14 @@
 #!/usr/bin/env runghc
-{-# LANGUAGE CPP #-}{-# LANGUAGE MultiWayIf #-}
-w = wordList !! ((`mod` length wordList) . read $ [x|x<-__TIME__,x/=':'])
-decode=(>>=(\x->triples!!((fromEnum x)-2^17)))
---- cycling through the whole list by period s.t. length mod period = 1
--- or: after 5 words: "Enough playing, go back to work!"
-
-chars = ' ':['a'..'z']
-triples = mapM id=<<[[chars,chars,chars],[chars,chars],[chars]] -- why only chars here? maybe use larger set and compress the hangman picture?
-
-wordList = words . decode $ "𣜈𡇄𠹇𠖱𤝈𠹬𡝡𠂔𠑔𣥻𢗻𣓙𠹀𢮞𢚦𢮍𠅮𢟽𠂷𢡃𢲝𠒙𢌢𠓾𣼭𠗛𠹐𠐽𠈂𢡥𡐗𠹉𠐽" -- 2^17 = 0x20000; U+20000 - początek długiego bloku CJK (U+20000 - U+2A6DF)
-
-h = "  ___\n |  \\|\n o   |\n-+-  |\n/ \\  |\n   __|__\n"
-m = "aadddaafaaecaagaaacaihjaacakalaacaaaabbcbba" -- use characters and compress!
-
-u k a b|b <= toEnum (k + 97)=a|1>0=' '
-
-main = game 0 ('.'<$w)
-
-f x a '.'|a==x=a|1>0='.'
-f _ _ b=b
-
-toLower c = if elem c ['A'..'Z'] then shift 32 c else c
-
-i a b c = if a then b else c
-
-shift n = toEnum . (+ n) . fromEnum
-
-game n a = do
-  putStrLn $ "\ESC[2J"++(zipWith (u n) h m)++'\n':a++"\n\n"
-  if | n == 11 -> putStrLn "You lose!"
-     | elem '.' a -> 
-      do
-        putStrLn $ "Choose a letter. Wrong guesses: " ++ (show n) ++ "/11."
-        ls <- ((fmap toLower)<$>) $ getLine
-        case ls of l:[] -> game (n + (fromEnum $ elem l ['a'..'z']) - (fromEnum $ elem l w)) (zipWith (f l) w a); _ -> game n a
-     | 1>0 -> putStrLn "You win!"
-
-{-
-  ___\n
- |  \|\n
- o   |\n
--+-  |\n
-/ \  |\n
-   __|__
-
-003330
-0500420
-0600020
-8790020
-:0;0020
-00011211 
--}
+{-#LANGUAGE CPP#-}{-#LANGUAGE MultiWayIf#-}{-#LANGUAGE LambdaCase#-}
+d=(>>=(\x->mapM id[c,c,c]!!((ḟ x)-272)));z=zipWith;main = h 0;p=putStrLn;r=p.d;f
+ x a '.'|a==x=a|j='.';f _ _ b=b;(#)=elem;ḟ=fromEnum;ţ=toEnum;c=['a'..'z']++" /?\
+.:YWEPCB";
+m=d"ēᆊǉƦĖĐொ⟌Ś㚕ĒĐĶ௨Đ"
+j=1>0;
+u k a b|b<=ţ(k+97)=a|j=' ';
+l=length w;
+e(-1)=[];e n|n==l=[]|1>0=[y|y<-w,not.elem y$e<$>[0..n-1]]!!(mod(read[y|y<-__TIME__,y/=':'])$l-n)
+b p q=r p>>((span(# q)<$>).(((\c->if c#['A'..'Z']then ţ.(+32).ḟ$c else c)<$>)<$>)$getLine)>>=(\case{(x:[],[])->pure x;_->b p q})
+g n a v=do{p$"\ESC[2J"++(z(u n)"  ___\n |  \\|\n o   |\n-+-  |\n/ \\  |\n   __|__\n "m)++a;p$d"꺳䞍␎揰旂"++(show n)++"/11.\n";if|n==11->r"꣱趿戃"|'.'#a->do{q<-b"덽ᤃӝ᥆ᤆ"['a'..'z'];g(n+1-(fromEnum$q#v))(z(f q)v a)v}|j->r"꣱轐䫠"}
+h n|n<l=do{g 0('.'<$e n)(e n);q<-b"롹蔪℮䪻蕜"['y','n'];case q of 'y'->h$n+1;_->return()}|j=r"델泩蹐Ґ䞐酎ൎ棋磍㫕"
+w=words.d$"扩Ბᨿӆ蕀ᚇ✤貺˱杙䉦帀ᨸ䳿䍍䳤跤䚘賧䛲乽͊㴍ϗ爋ٶᩈː躴䝃Ⅎᩁː"
