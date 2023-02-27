38d94c49afcb28cc40ce393d588a1b83445eb292
Michal Kukiela <michal.kukiela@mga.com.pl>
2023-02-28 00:15:52 +0100

    golfing some more...

--- prelude/hangman/hangman.hs
+++ prelude/hangman/hangman.hs
@@ -1,14 +1,11 @@
-#!/usr/bin/env runghc
-{-#LANGUAGE CPP#-}{-#LANGUAGE MultiWayIf#-}{-#LANGUAGE LambdaCase#-}
+#!/usr/bin/env -S runghc -XCPP -XMultiWayIf -XLambdaCase
 d=(>>=(\x->mapM id[c,c,c]!!((ḟ x)-272)));z=zipWith;main = h 0;p=putStrLn;r=p.d;f
  x a '.'|a==x=a|j='.';f _ _ b=b;(#)=elem;ḟ=fromEnum;ţ=toEnum;c=['a'..'z']++" /?\
-.:YWEPCB";
-m=d"ēᆊǉƦĖĐொ⟌Ś㚕ĒĐĶ௨Đ"
-j=1>0;
-u k a b|b<=ţ(k+97)=a|j=' ';
-l=length w;
+.:YWEPCB";m=d"ēᆊǉƦĖĐொ⟌Ś㚕ĒĐĶ௨Đ";j=1>0;u k a b|b<=ţ(k+97)=a|j=' ';l=length w;h n|\
+n<l=do{g 0('.'<$e n)(e n);q<-b"롹蔪℮䪻蕜"['y','n'];case q of 'y'->h$n+1;_->pure()}|j=r"델泩蹐Ґ䞐酎ൎ棋磍㫕"
+b p q=r p>>((span(# q)<$>).(((\c->if|c#['A'..'Z']->ţ.(+32).ḟ$c|j->c)<$>)<$>)$getLine)>>=(\case{(x:[],[])->pure x;_->b p q})
 e(-1)=[];e n|n==l=[]|1>0=[y|y<-w,not.elem y$e<$>[0..n-1]]!!(mod(read[y|y<-__TIME__,y/=':'])$l-n)
-b p q=r p>>((span(# q)<$>).(((\c->if c#['A'..'Z']then ţ.(+32).ḟ$c else c)<$>)<$>)$getLine)>>=(\case{(x:[],[])->pure x;_->b p q})
+
 g n a v=do{p$"\ESC[2J"++(z(u n)"  ___\n |  \\|\n o   |\n-+-  |\n/ \\  |\n   __|__\n "m)++a;p$d"꺳䞍␎揰旂"++(show n)++"/11.\n";if|n==11->r"꣱趿戃"|'.'#a->do{q<-b"덽ᤃӝ᥆ᤆ"['a'..'z'];g(n+1-(fromEnum$q#v))(z(f q)v a)v}|j->r"꣱轐䫠"}
-h n|n<l=do{g 0('.'<$e n)(e n);q<-b"롹蔪℮䪻蕜"['y','n'];case q of 'y'->h$n+1;_->return()}|j=r"델泩蹐Ґ䞐酎ൎ棋磍㫕"
+
 w=words.d$"扩Ბᨿӆ蕀ᚇ✤貺˱杙䉦帀ᨸ䳿䍍䳤跤䚘賧䛲乽͊㴍ϗ爋ٶᩈː躴䝃Ⅎᩁː"
